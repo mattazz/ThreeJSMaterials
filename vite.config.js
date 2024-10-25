1d191ed9d1aeb39c1517d1f6ed5f9cd173484a25
Mattazz <matthewazada101@gmail.com>
2024-10-25 15:09:28 -0400

    updated base path

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import restart from 'vite-plugin-restart'
 
 export default {
-    base: '/learning3JSMaterials/', // Base URL for GitHub Pages
+    base: '/ThreeJSMaterials/', // Base URL for GitHub Pages
     root: 'src/', // Sources files (typically where index.html is)
     publicDir: '../static/', // Path from "root" to static assets (files that are served as they are)
     server:
